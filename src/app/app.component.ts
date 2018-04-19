4815cc37012876322a28b842df3e46ad335333d5
Paul Gualotuna <paul.gualotuna@tum.de>
2018-04-19 13:11:06 +0200

    Add Logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public title = 'Gualotuna.com';
   public showMenu = false;
 
   public manageMenu() {
