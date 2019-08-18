abe7a2ec98861016c70f3ceac049d9de1d5e4c7f
Paul Gualotuna <paul.gualotuna.dev@gmail.com>
2019-08-18 10:17:16 +0200

    feat(blog): add blog overview cards

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {graphql} from 'react-apollo';
 import gql from 'graphql-tag';
+import {Item} from "semantic-ui-react";
 import {withGlobalState} from "react-globally";
 
 class Blog extends React.Component {
@@ -23,13 +24,22 @@ class Blog extends React.Component {
             return <div>Loading blog...</div>
         } else {
             return <div>
-                <div>All entries</div>
-                <div>{
-                    data.posts.map((post, i) =>
-                        <div key={i} onClick={() => this.goToPost(post.slug)}>{JSON.stringify(post)}</div>
-                    )
-                }
-                </div>
+                <h1>All entries</h1>
+                <Item.Group divided>
+                    {
+                        data.posts.map((post, i) =>
+                            <Item key={i} onClick={() => this.goToPost(post.slug)}>
+                                <Item.Image size='small' src='./assets/photo-card.jpg'/>
+                                <Item.Content>
+                                    <Item.Header>{post.title}</Item.Header>
+                                    <Item.Meta>{post.date}</Item.Meta>
+                                    <Item.Meta>{post.slug}</Item.Meta>
+                                    <Item.Description>{post.body}</Item.Description>
+                                </Item.Content>
+                            </Item>
+                        )
+                    }
+                </Item.Group>
             </div>
         }
     }
