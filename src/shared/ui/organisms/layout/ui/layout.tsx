b4a4d4240642395656cc3a193bb75ab9cadce5ed
Paul Gualotuna <paul.gualotuna.dev@gmail.com>
2026-03-16 23:43:30 +0100

    fix(mobile): fix sidebar toggle, fix snap (#37)

--- src/shared/ui/organisms/layout/ui/layout.tsx
+++ src/shared/ui/organisms/layout/ui/layout.tsx
@@ -9,7 +9,7 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <div className='flex flex-col dark:bg-gray-950'>
       <Sidebar />
-      <main className='h-dvh w-full overflow-y-scroll snap-y snap-mandatory md:pl-14'>{children}</main>
+      <main className='h-dvh w-full overflow-y-scroll snap-y snap-proximity md:snap-mandatory md:pl-14'>{children}</main>
     </div>
   )
 }
